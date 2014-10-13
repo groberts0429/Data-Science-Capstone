534539413f5c72c112b8410316a54fa4b9ce4629
George Roberts III <groberts0429@gmail.com>
2014-10-13 05:52:04 -0400

    Update ui.R
    Single output version (later versions will allow three outputs).

--- ui.R
+++ ui.R
@@ -1,1 +1,15 @@
-empty
+shinyUI(fluidPage (
+  titlePanel("TextPredictor"),
+  sidebarLayout(
+    sidebarPanel(
+      textInput("inputBoxId", "Input", value="<start>"), #textInput(inputId, label, value = "")
+      br(),
+      div(textOutput("outputBoxId", container = span), style=boxstyle)
+    ),
+    mainPanel(
+      strong("Instructions"),
+      br(),
+      p("Type one, or two-word input in the 'Input' box.")
+    )
+  )
+))
